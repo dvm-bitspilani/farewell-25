2edfb55bd85cf5f449753e0258602541673651e3
Pranjal Gupta <pranjalgupta58.97@gmail.com>
2025-11-28 15:00:29 +0530

    war started

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import styles from "./App.module.scss";
 import Page from "./components/Page/Page";
+import WarMap from "./components/WarMap/WarMap";
 
 function App() {
   return <Page /> ;
+  // return <WarMap />;
 }
 
 export default App;
